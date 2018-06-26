82ca375b7b93bb4e23f72f9329ba64e3024aad7d
Administrator <administrator@gbtc408013dt.local>
2018-06-26 14:27:25 -0400

    Initial Commit

---
+++ Meal Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meal Tracker
+//
+//  Created by Administrator on 6/26/18.
+//  Copyright © 2018 Administrator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
